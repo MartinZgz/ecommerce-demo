9e917daa20bcd41729ef660a114885b327dc0c1c
Martín <martin1996zgz@gmail.com>
2023-09-06 21:20:51 +0200

    Create Spring-boot project

---
+++ src/test/java/com/agile/monkeys/eCommerceDemo/ECommerceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agile.monkeys.eCommerceDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
